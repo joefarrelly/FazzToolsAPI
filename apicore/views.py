cf1e0e8e6df5a365aaf8213d1aa568c5f91c389b
Joseph Farrelly <joefarrelly96@gmail.com>
2022-11-26 13:41:21 +0000

    fixes

--- apicore/views.py
+++ apicore/views.py
@@ -158,9 +158,13 @@ class ProfileUserView(viewsets.ModelViewSet):
         else:
             if queryset[0].userFile:
                 temp = queryset[0].userFile
-                with open(temp.file, 'r') as file:
-                    all_lines = [line.decode('utf-8') for line in file]
+                temp2 = temp.file.open('r')
+                # temp3 = temp2.read()
+                temp3 = temp2.readlines()
                 # temp6 = temp3.decode('utf-8')
+                # print(repr(temp3))
+                all_lines = [line.decode('utf-8') for line in temp3]
+                temp.file.close()
                 temp4 = recursive()
                 temp7 = temp4[1]
                 # print(temp6)
