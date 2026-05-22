2918bff2dae3b898e5dff2980e5169599ca8c94f
Joe Farrelly <joefarrelly96@gmail.com>
2026-05-23 00:45:16 +0100

    Enable Celery acks_late and reject_on_worker_lost

--- backend/settings.py
+++ backend/settings.py
@@ -92,6 +92,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://redis:6379/0")
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_TASK_ACKS_LATE = True
+CELERY_TASK_REJECT_ON_WORKER_LOST = True
 
 CELERY_TASK_ROUTES = {
     "apicore.tasks.scan_single_alt": {"queue": "alt_scan"},
